bbd91ea0616902d2bc3c2f8a8e87e68bb91b7c8c
Minurte1 <hohoangphucjob@gmail.com>
2024-03-15 21:21:33 +0700

    step 12 - Thực hành if else elseif

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ echo $ten . $namsinh ."hello <h1>worlkádasd</h1> ";
 define("tenn","phucne");
 echo "In hằng ra" . tenn;
 
+//step 11 Câu lệnh if else elseif
 $a = 15;
 if( $a <10){
     echo "a nho hon 10";
@@ -28,6 +29,16 @@ elseif ($a< 30){
     echo "a nho hon 30";
 }else{
     echo "ulatr";
+}
+
+//step 12 Thực hành if else elseif
+
+$ten_nguoi_dung = "phuc";
+if($ten_nguoi_dung == "phuc"){
+    echo "Người này tên là phúc";
+}
+else{
+    echo "Người này không phải tên phúc"
 }
 ?>
 </body>
